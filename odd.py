12dea0a2dbf7a3ee4ef18316b901ae85b0d4a2db
dborta <dborta@yahoo.com>
2019-01-26 16:46:58 +0530

    exception handling

--- odd.py
+++ odd.py
@@ -1,5 +1,15 @@
-a = int(input("enter a number"))
-if a%2==0:
-	print(a, "is even")
-else:
-	print(a, "is odd")
+a = input("enter a number")
+
+
+try :
+	a = int(a)
+	if a%2==0:
+		print(a, "is even")
+	else:
+		print(a, "is odd")
+except ValueError:
+	print("plz enter num") 
+
+
+
+
